e501629bfdbbd0916af5424e86d0272e0da075a0
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-24 13:02:22 -0300

    added javaScript

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const monthlyPrices = document.querySelectorAll(".planValue__month");
+
+const annualPrices = document.querySelectorAll(".planValue__year");
+
+toggle.addEventListener("change", function () {
+  monthlyPrices.forEach((monthlyPrice) => {
+    monthlyPrice.style.display = toggle.checked == true ? "block" : "none";
+  });
+  annualPrices.forEach((annualPrice) => {
+    annualPrice.style.display = toggle.checked == true ? "none" : "block";
+  });
+});
