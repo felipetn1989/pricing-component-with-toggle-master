42301004a22479f4f0425daf976e2f3d52ff1345
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-24 13:44:51 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ const annualPrices = document.querySelectorAll(".planValue__year");
 
 toggle.checked == true;
 
+/* First, the code calls a function everytime the toggle changes. This function will execute a forEach for all the price spans and display the correct price for each position of toggle */
+
 toggle.addEventListener("change", function () {
   monthlyPrices.forEach((monthlyPrice) => {
     monthlyPrice.style.display = toggle.checked == true ? "block" : "none";
