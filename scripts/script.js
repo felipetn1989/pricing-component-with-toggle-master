f684263054b0365ef5d9fc6007878dac39b3d7d6
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-24 13:42:13 -0300

    v1 finished

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ const monthlyPrices = document.querySelectorAll(".planValue__month");
 
 const annualPrices = document.querySelectorAll(".planValue__year");
 
+toggle.checked == true;
+
 toggle.addEventListener("change", function () {
   monthlyPrices.forEach((monthlyPrice) => {
     monthlyPrice.style.display = toggle.checked == true ? "block" : "none";
